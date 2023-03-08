70ddc218947ea05bb3ba99b1ab5d18d576c8c56c
MomusWinner <areg06.06@mail.ru>
2023-03-08 14:56:26 +0300

    first commit

---
+++ Assets/Scenes/Scripts/EditorCardboardCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorCardboardCamera : MonoBehaviour
+{
+#if UNITY_EDITOR
+Vector2 rotation = Vector2.zero;
+public float speed = 3;
+
+void Update()
+{
+if (Input.GetKey(KeyCode.LeftAlt))
+{
+rotation.y += Input.GetAxis("Mouse X");
+rotation.x += -Input.GetAxis("Mouse Y");
+transform.eulerAngles = (Vector2)rotation * speed;
+}
+}
+#endif
+}
